cd5f9b8bda893899be645ab96542a3a085e3491e
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-08-04 16:37:23 +0300

    Initial Commit

---
+++ MVC-N/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Oleg Tsarenkoff on 4.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
